9642466025e2367002df08bad5a4693e44af5053
bsokat <barissokat@gmail.com>
2017-02-27 20:29:24 +0300

    Initial commit

---
+++ WebGrid/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebGrid.Startup))]
+namespace WebGrid
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
