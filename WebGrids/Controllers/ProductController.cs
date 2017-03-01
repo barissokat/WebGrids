2afd6b2e7cf1cc393ec4b660ed70dc8220fa68bc
bsokat <barissokat@gmail.com>
2017-03-01 08:41:35 +0300

    Upgraded Paging

--- WebGrids/Controllers/ProductController.cs
+++ WebGrids/Controllers/ProductController.cs
@@ -17,7 +17,9 @@ namespace WebGrids.Controllers
                 new Product { ID = 1, Name = "PC", Price = 1200, Count = 200 },
                 new Product { ID = 2, Name = "Laptop", Price = 1300, Count = 300 },
                 new Product { ID = 3, Name = "LCD", Price = 1400, Count = 400 },
-                new Product { ID = 4, Name = "LED", Price = 1500, Count = 500 }
+                new Product { ID = 4, Name = "LED", Price = 1500, Count = 500 },
+                new Product { ID = 5, Name = "Smart", Price = 1600, Count = 600 },
+                new Product { ID = 6, Name = "Tablet", Price = 1700, Count = 700 }
             };
             return View(products);
         }
